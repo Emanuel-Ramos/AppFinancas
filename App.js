f7ecfcc81d81f678f6b5be503584c6b045f79ee8
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-02-12 13:09:14 -0300

    seccond commit

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
  return (
     <NavigationContainer>
       <AuthProvider>
-        <StatusBar backgroundColor='black'/>
+        <StatusBar backgroundColor='#121110'/>
         <Routes/>
       </AuthProvider>
     </NavigationContainer>
